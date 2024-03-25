179b69d1059ff741b524129a9509efa75d929062
Stefan Haller <stefan@haller-berlin.de>
2024-03-25 18:06:46 +0100

    WIP Add version when deploying
    Just so that people see the breaking changes popup this time. Might remove this
    again.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,12 +9,14 @@ test -d $server || {
     exit 1
 }
 
-GOOS=darwin GOARCH=amd64 go build -o lazygit_amd64
-GOOS=darwin GOARCH=arm64 go build -o lazygit_arm64
+VERSION=0.53.0
+
+GOOS=darwin GOARCH=amd64 go build -o lazygit_amd64 -ldflags="-X 'main.version=$VERSION'"
+GOOS=darwin GOARCH=arm64 go build -o lazygit_arm64 -ldflags="-X 'main.version=$VERSION'"
 lipo -create -output lazygit lazygit_amd64 lazygit_arm64
 rm lazygit_amd64 lazygit_arm64
 
-GOOS=windows GOARCH=amd64 go build
+GOOS=windows GOARCH=amd64 go build -ldflags="-X 'main.version=$VERSION'"
 
 mv lazygit $server/Mac
 mv lazygit.exe $server/Windows
